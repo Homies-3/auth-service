9876cd199e5aded631302e2a2f73864cdfe23f1a
asishshaji <asishshajithomas@gmail.com>
2023-04-16 11:38:06 +0530

    added controllers

--- controller.js
+++ controller.js
@@ -13,9 +13,31 @@ let validate = (validateMessage, callback) => {
     })
   }
 
-let register = (registerMessage, callback) => {}
+let register = (registerMessage, callback) => {
+    var phone = registerMessage.request.phone;
+    var password = registerMessage.request.password;
+    var company = registerMessage.request.company;
 
-let login = (loginMessage, callback) => {}
+    // write the logic in service file
+
+    callback(null,{
+        "status":200,
+        "error":"some error message"
+    })
+}
+
+let login = (loginMessage, callback) => {
+    var username = loginMessage.request.username;
+    var password = loginMessage.request.password;
+
+    //write the logic in service file
+
+    callback(null, {
+        "status":200,
+        "error": "some error message",
+        "token":"access_token"
+    })
+}
 
 module.exports = {validate,register,login}
 
